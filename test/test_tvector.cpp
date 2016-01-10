95b16f036a4ddf85c2dcc4d627895e927f646883
sinitskaya <o.sinitskayaf@yandex.ru>
2016-01-10 17:18:43 +0300

    My tests

--- test/test_tvector.cpp
+++ test/test_tvector.cpp
@@ -193,3 +193,90 @@ TEST(TVector, cant_multiply_vectors_with_not_equal_size)
 	TVector<int> v2(3);
 	ASSERT_ANY_THROW(v1 * v2);
 }
+
+
+//////
+TEST(TVector, throws_when_create_vector_with_startindex_bigger_than_size)
+{
+	ASSERT_ANY_THROW(TVector<int> v(4, 5));
+}
+
+TEST(TVector, throws_when_set_element_with_index_smaller_than_startindex)
+{
+	TVector<int> v(3, 1);
+	ASSERT_ANY_THROW(v[0] = 1);
+}
+
+TEST(TVector, throws_when_set_element_with_index_bigger_than_size_plus_startindex)
+{
+	TVector<int> v(3, 1);
+	ASSERT_ANY_THROW(v[100] = 1);
+}
+
+TEST(TVector, assign_operator_change_vector)
+{
+	TVector<int> v1(5);
+	TVector<int> v2(3);
+	TVector<int> t(v1);
+	v1 = v2;
+	EXPECT_NE(t, v1);
+}
+
+TEST(TVector, assign_operator_change_startindex)
+{
+	TVector<int> v1(3, 1);
+	TVector<int> v2(3, 2);
+	int i = v1.GetStartIndex();
+	v1 = v2;
+	EXPECT_NE(i, v1.GetStartIndex());
+}
+
+TEST(TVector, compare_not_equal_vectors_return_true)
+{
+	TVector<int> v1(5);
+	TVector<int> v2(3);
+	EXPECT_EQ(true, (v2 != v1));
+}
+
+TEST(TVector, vectors_with_different_startindex_are_not_equal)
+{
+	TVector<int> v1(5,1);
+	TVector<int> v2(5,2);
+	EXPECT_NE(v1, v2);
+}
+
+TEST(TVector, cant_add_vectors_with_not_equal_startindex)
+{
+	TVector<int> v1(5,1);
+	v1[1] = 1;
+	v1[2] = 2;
+	TVector<int> v2(5,2);
+	v2[2] = 3;
+	v2[3] = 4;
+	v2[4] = 5;
+	ASSERT_ANY_THROW(v1 + v2);
+}
+
+TEST(TVector, cant_subtract_vectors_with_not_equal_startindex)
+{
+	TVector<int> v1(5,1);
+	v1[1] = 1;
+	v1[2] = 2;
+	TVector<int> v2(5,2);
+	v2[2] = 3;
+	v2[3] = 4;
+	v2[4] = 5;
+	ASSERT_ANY_THROW(v1 - v2);
+}
+
+TEST(TVector, cant_multiply_vectors_with_not_equal_startindex)
+{
+	TVector<int> v1(5,1);
+	v1[1] = 1;
+	v1[2] = 2;
+	TVector<int> v2(5,2);
+	v2[2] = 3;
+	v2[3] = 4;
+	v2[4] = 5;
+	ASSERT_ANY_THROW(v1 * v2);
+}
