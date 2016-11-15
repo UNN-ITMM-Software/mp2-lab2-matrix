00872ce9152320838d9c9f798b8e12153527fcd4
Alexandr Konakov <konakov@my.com>
2016-11-15 18:41:59 +0300

    Внесены правки по cpplint'у

--- test/test_tmatrix.cpp
+++ test/test_tmatrix.cpp
@@ -1,145 +1,101 @@
+// Copyright 2016 AuthorName. All rights reserved.
 #include "utmatrix.h"
-
 #include <gtest.h>
-
-TEST(TMatrix, can_create_matrix_with_positive_length)
-{
-	ASSERT_NO_THROW(TMatrix<int> m(5));
+TEST(TMatrix, can_create_matrix_with_positive_length) {
+  ASSERT_NO_THROW(TMatrix<int> m(5));
 }
-
-TEST(TMatrix, cant_create_too_large_matrix)
-{
-	ASSERT_ANY_THROW(TMatrix<int> m(MAX_MATRIX_SIZE + 1));
+TEST(TMatrix, cant_create_too_large_matrix) {
+  ASSERT_ANY_THROW(TMatrix<int> m(MAX_MATRIX_SIZE + 1));
 }
-
-TEST(TMatrix, throws_when_create_matrix_with_negative_length)
-{
-	ASSERT_ANY_THROW(TMatrix<int> m(-5));
+TEST(TMatrix, throws_when_create_matrix_with_negative_length) {
+  ASSERT_ANY_THROW(TMatrix<int> m(-5));
 }
-
-TEST(TMatrix, can_create_copied_matrix)
-{
-	TMatrix<int> m(5);
-
-	ASSERT_NO_THROW(TMatrix<int> m1(m));
+TEST(TMatrix, can_create_copied_matrix) {
+  TMatrix<int> m(5);
+  ASSERT_NO_THROW(TMatrix<int> m1(m));
 }
-
-TEST(TMatrix, copied_matrix_is_equal_to_source_one)
-{
-	TMatrix<int> matr(5), copyM;
-	matr[1][1] = 199;
-	TMatrix<int> copyMatr(matr);
-	EXPECT_TRUE(matr[1][1] == copyMatr[1][1]);
+TEST(TMatrix, copied_matrix_is_equal_to_source_one) {
+  TMatrix<int> matr(5), copyM;
+  matr[1][1] = 199;
+  TMatrix<int> copyMatr(matr);
+  EXPECT_TRUE(matr[1][1] == copyMatr[1][1]);
 }
-
-TEST(TMatrix, copied_matrix_has_its_own_memory)
-{
-	TMatrix<int> matr(5);
-	TMatrix<int> copyMatr(5);
-	copyMatr[1][1] = 199;
-	EXPECT_TRUE(matr[1][1] != copyMatr[1][1]);
+TEST(TMatrix, copied_matrix_has_its_own_memory) {
+  TMatrix<int> matr(5);
+  TMatrix<int> copyMatr(5);
+  copyMatr[1][1] = 199;
+  EXPECT_TRUE(matr[1][1] != copyMatr[1][1]);
 }
-
-TEST(TMatrix, can_get_size)
-{
-	TMatrix<int> matr(10);
-	ASSERT_NO_THROW(matr.GetSize());
+TEST(TMatrix, can_get_size) {
+  TMatrix<int> matr(10);
+  ASSERT_NO_THROW(matr.GetSize());
 }
-
-TEST(TMatrix, can_set_and_get_element)
-{
-	TMatrix<int> matr(10);
-	ASSERT_NO_THROW(matr[0][0] = 7);
-	ASSERT_NO_THROW(matr[0][0]);
+TEST(TMatrix, can_set_and_get_element) {
+  TMatrix<int> matr(10);
+  ASSERT_NO_THROW(matr[0][0] = 7);
+  ASSERT_NO_THROW(matr[0][0]);
 }
-
-TEST(TMatrix, throws_when_set_element_with_negative_index)
-{
-	TMatrix<int> matr(10);
-	ASSERT_ANY_THROW(matr[-4][5]);
+TEST(TMatrix, throws_when_set_element_with_negative_index) {
+  TMatrix<int> matr(10);
+  ASSERT_ANY_THROW(matr[-4][5]);
 }
-
-TEST(TMatrix, throws_when_set_element_with_too_large_index)
-{
-	TMatrix<int> matr(10);
-	ASSERT_ANY_THROW(matr[20][5] = 10);
+TEST(TMatrix, throws_when_set_element_with_too_large_index) {
+  TMatrix<int> matr(10);
+  ASSERT_ANY_THROW(matr[20][5] = 10);
 }
-
-TEST(TMatrix, can_assign_matrix_to_itself)
-{
-	TMatrix<int> matr(10);
-	matr[3][6] = 7;
-	ASSERT_NO_THROW(matr = matr);
+TEST(TMatrix, can_assign_matrix_to_itself) {
+  TMatrix<int> matr(10);
+  matr[3][6] = 7;
+  ASSERT_NO_THROW(matr = matr);
 }
-
-TEST(TMatrix, can_assign_matrices_of_equal_size)
-{
-	TMatrix<int> matr(10), assignable(10);
-	matr[4][4] = 12;
-	ASSERT_NO_THROW(assignable = matr);
+TEST(TMatrix, can_assign_matrices_of_equal_size) {
+  TMatrix<int> matr(10), assignable(10);
+  matr[4][4] = 12;
+  ASSERT_NO_THROW(assignable = matr);
 }
-
-TEST(TMatrix, assign_operator_change_matrix_size)
-{
-	TMatrix<int> matr(10), assignable(5);
-	EXPECT_NE(matr.GetSize(), assignable.GetSize());
-	ASSERT_NO_THROW(assignable = matr);
-	EXPECT_EQ(matr.GetSize(), assignable.GetSize());
+TEST(TMatrix, assign_operator_change_matrix_size) {
+  TMatrix<int> matr(10), assignable(5);
+  EXPECT_NE(matr.GetSize(), assignable.GetSize());
+  ASSERT_NO_THROW(assignable = matr);
+  EXPECT_EQ(matr.GetSize(), assignable.GetSize());
 }
-
-TEST(TMatrix, can_assign_matrices_of_different_size)
-{
-	TMatrix<int> matr(10), assignable(5);
-	ASSERT_NO_THROW(assignable = matr);
+TEST(TMatrix, can_assign_matrices_of_different_size) {
+  TMatrix<int> matr(10), assignable(5);
+  ASSERT_NO_THROW(assignable = matr);
 }
-
-TEST(TMatrix, compare_equal_matrices_return_true)
-{
-	TMatrix<int> matr(10),  assignable(5);
-	matr[5][5] = 3829;
-	assignable = matr;
-	EXPECT_TRUE(assignable == matr);
+TEST(TMatrix, compare_equal_matrices_return_true) {
+  TMatrix<int> matr(10),  assignable(5);
+  matr[5][5] = 3829;
+  assignable = matr;
+  EXPECT_TRUE(assignable == matr);
 }
-
-TEST(TMatrix, compare_matrix_with_itself_return_true)
-{
-	TMatrix<int> matr(10);
-	matr[5][5] = 3829;
-	EXPECT_TRUE(matr == matr);
+TEST(TMatrix, compare_matrix_with_itself_return_true) {
+  TMatrix<int> matr(10);
+  matr[5][5] = 3829;
+  EXPECT_TRUE(matr == matr);
 }
-
-TEST(TMatrix, matrices_with_different_size_are_not_equal)
-{
-	TMatrix<int> matr(10),  assignable(5);
-	matr[1][1] = assignable[1][1] = 3829;
-	EXPECT_TRUE(assignable != matr);
+TEST(TMatrix, matrices_with_different_size_are_not_equal) {
+  TMatrix<int> matr(10),  assignable(5);
+  matr[1][1] = assignable[1][1] = 3829;
+  EXPECT_TRUE(assignable != matr);
 }
-
-TEST(TMatrix, can_add_matrices_with_equal_size)
-{
-	TMatrix<int> matr(10), sub(10);
-	matr[7][7] = 7;
-	ASSERT_NO_THROW(matr - sub);
+TEST(TMatrix, can_add_matrices_with_equal_size) {
+  TMatrix<int> matr(10), sub(10);
+  matr[7][7] = 7;
+  ASSERT_NO_THROW(matr - sub);
 }
-
-TEST(TMatrix, cant_add_matrices_with_not_equal_size)
-{
-	TMatrix<int> matr(10),  assignable(5);
-	matr[1][1] = assignable[1][1] = 3829;
-	ASSERT_ANY_THROW(assignable + matr);
+TEST(TMatrix, cant_add_matrices_with_not_equal_size) {
+  TMatrix<int> matr(10),  assignable(5);
+  matr[1][1] = assignable[1][1] = 3829;
+  ASSERT_ANY_THROW(assignable + matr);
 }
-
-TEST(TMatrix, can_subtract_matrices_with_equal_size)
-{
-	TMatrix<int> matr(10), sub(10);
-	matr[7][7] = 7;
-	ASSERT_NO_THROW(matr - sub);
+TEST(TMatrix, can_subtract_matrices_with_equal_size) {
+  TMatrix<int> matr(10), sub(10);
+  matr[7][7] = 7;
+  ASSERT_NO_THROW(matr - sub);
 }
-
-TEST(TMatrix, cant_subtract_matrixes_with_not_equal_size)
-{
-	TMatrix<int> matr(10),  assignable(5);
-	matr[1][1] = assignable[1][1] = 3829;
-	ASSERT_ANY_THROW(assignable = assignable - matr);
+TEST(TMatrix, cant_subtract_matrixes_with_not_equal_size) {
+  TMatrix<int> matr(10),  assignable(5);
+  matr[1][1] = assignable[1][1] = 3829;
+  ASSERT_ANY_THROW(assignable = assignable - matr);
 }
-
--- test/test_tvector.cpp
+++ test/test_tvector.cpp
@@ -1,240 +1,176 @@
+// Copyright 2016 AuthorName. All rights reserved.
 #include "utmatrix.h"
-
 #include <gtest.h>
-
-TEST(TVector, can_create_vector_with_positive_length)
-{
+TEST(TVector, can_create_vector_with_positive_length) {
   ASSERT_NO_THROW(TVector<int> v(5));
 }
-
-TEST(TVector, cant_create_too_large_vector)
-{
+TEST(TVector, cant_create_too_large_vector) {
   ASSERT_ANY_THROW(TVector<int> v(MAX_VECTOR_SIZE + 1));
 }
-
-TEST(TVector, throws_when_create_vector_with_negative_length)
-{
+TEST(TVector, throws_when_create_vector_with_negative_length) {
   ASSERT_ANY_THROW(TVector<int> v(-5));
 }
-
-TEST(TVector, throws_when_create_vector_with_negative_startindex)
-{
+TEST(TVector, throws_when_create_vector_with_negative_startindex) {
   ASSERT_ANY_THROW(TVector<int> v(5, -2));
 }
-
-TEST(TVector, can_create_copied_vector)
-{
+TEST(TVector, can_create_copied_vector) {
   TVector<int> v(10);
-
   ASSERT_NO_THROW(TVector<int> v1(v));
 }
-
-TEST(TVector, copied_vector_is_equal_to_source_one)
-{
-	TVector<int> f(10);
-	f[4] = 7;
-	TVector<int> s(f);
-	EXPECT_EQ(f, s);
+TEST(TVector, copied_vector_is_equal_to_source_one) {
+  TVector<int> f(10);
+  f[4] = 7;
+  TVector<int> s(f);
+  EXPECT_EQ(f, s);
 }
-
-TEST(TVector, copied_vector_has_its_own_memory)
-{	
-	TVector<int> f(10);
-	TVector<int> s(f);
-	f[6] = 4;
-	EXPECT_NE(f[6], s[6]);
+TEST(TVector, copied_vector_has_its_own_memory) {
+  TVector<int> f(10);
+  TVector<int> s(f);
+  f[6] = 4;
+  EXPECT_NE(f[6], s[6]);
 }
-
-TEST(TVector, can_get_size)
-{
+TEST(TVector, can_get_size) {
   TVector<int> v(4);
-
   EXPECT_EQ(4, v.GetSize());
 }
-
-TEST(TVector, can_get_start_index)
-{
+TEST(TVector, can_get_start_index) {
   TVector<int> v(4, 2);
-
   EXPECT_EQ(2, v.GetStartIndex());
 }
-
-TEST(TVector, can_set_and_get_element)
-{
+TEST(TVector, can_set_and_get_element) {
   TVector<int> v(4);
   v[0] = 4;
-
   EXPECT_EQ(4, v[0]);
 }
-
-TEST(TVector, throws_when_set_element_with_negative_index)
-{
-	TVector<int> v(4);
-	ASSERT_ANY_THROW(v[-1] = 7);
+TEST(TVector, throws_when_set_element_with_negative_index) {
+  TVector<int> v(4);
+  ASSERT_ANY_THROW(v[-1] = 7);
 }
-
-TEST(TVector, throws_when_set_element_with_too_large_index)
-{
-	TVector<int> v(4);
-	ASSERT_ANY_THROW(v[7] = 7);
+TEST(TVector, throws_when_set_element_with_too_large_index) {
+  TVector<int> v(4);
+  ASSERT_ANY_THROW(v[7] = 7);
 }
-
-TEST(TVector, can_assign_vector_to_itself)
-{
-	TVector<int> v(4);
-	ASSERT_NO_THROW(v = v);
+TEST(TVector, can_assign_vector_to_itself) {
+  TVector<int> v(4);
+  ASSERT_NO_THROW(v = v);
 }
-
-TEST(TVector, can_assign_vectors_of_equal_size)
-{
-	TVector<int> v1(4), v2(4);
-	EXPECT_EQ(v1.GetSize(), v2.GetSize());
-	v1[0] = 7;
-	v2 = v1;
-	EXPECT_EQ(v1.GetSize(), v2.GetSize());
-	for(int i = 0; i < v1.GetSize(); i++)
-		EXPECT_EQ(v1[i], v2[i]);
+TEST(TVector, can_assign_vectors_of_equal_size) {
+  TVector<int> v1(4), v2(4);
+  EXPECT_EQ(v1.GetSize(), v2.GetSize());
+  v1[0] = 7;
+  v2 = v1;
+  EXPECT_EQ(v1.GetSize(), v2.GetSize());
+  for (int i = 0; i < v1.GetSize(); i++)
+    EXPECT_EQ(v1[i], v2[i]);
 }
-
-TEST(TVector, assign_operator_change_vector_size)
-{
-  	TVector<int> v1(7), v2(4);
-	EXPECT_EQ(4, v2.GetSize());
-	EXPECT_EQ(v1.GetSize(), 7);
-	v1[0] = 7;
-	v2 = v1;
-	EXPECT_EQ(v1.GetSize(), v2.GetSize());
+TEST(TVector, assign_operator_change_vector_size) {
+  TVector<int> v1(7), v2(4);
+  EXPECT_EQ(4, v2.GetSize());
+  EXPECT_EQ(v1.GetSize(), 7);
+  v1[0] = 7;
+  v2 = v1;
+  EXPECT_EQ(v1.GetSize(), v2.GetSize());
 }
-
-TEST(TVector, can_assign_vectors_of_different_size)
-{
-  	TVector<int> v1(7), v2(4);
-	EXPECT_EQ(4, v2.GetSize());
-	EXPECT_EQ(v1.GetSize(), 7);
-	v1[0] = 7;
-	v2 = v1;
-	EXPECT_EQ(v1.GetSize(), v2.GetSize());
-	for(int i = 0; i < v1.GetSize(); i++)
-		EXPECT_EQ(v1[i], v2[i]);
+TEST(TVector, can_assign_vectors_of_different_size) {
+  TVector<int> v1(7), v2(4);
+  EXPECT_EQ(4, v2.GetSize());
+  EXPECT_EQ(v1.GetSize(), 7);
+  v1[0] = 7;
+  v2 = v1;
+  EXPECT_EQ(v1.GetSize(), v2.GetSize());
+  for (int i = 0; i < v1.GetSize(); i++)
+    EXPECT_EQ(v1[i], v2[i]);
 }
-
-TEST(TVector, compare_equal_vectors_return_true)
-{
-	TVector<int> v1(4), v2(4);
-	v2[0] = 14;
-	v1 = v2;
-	EXPECT_TRUE(v1 == v2);
+TEST(TVector, compare_equal_vectors_return_true) {
+  TVector<int> v1(4), v2(4);
+  v2[0] = 14;
+  v1 = v2;
+  EXPECT_TRUE(v1 == v2);
 }
-
-TEST(TVector, compare_vector_with_itself_return_true)
-{
-	TVector<int> v1(4);
-	EXPECT_TRUE(v1 == v1);
+TEST(TVector, compare_vector_with_itself_return_true) {
+  TVector<int> v1(4);
+  EXPECT_TRUE(v1 == v1);
 }
-
-TEST(TVector, vectors_with_different_size_are_not_equal)
-{
-	TVector<int> v1(4), v2(7);
-	v2[0] = v1[0] = 14;
-	EXPECT_FALSE(v1 == v2);
+TEST(TVector, vectors_with_different_size_are_not_equal) {
+  TVector<int> v1(4), v2(7);
+  v2[0] = v1[0] = 14;
+  EXPECT_FALSE(v1 == v2);
 }
-
-TEST(TVector, can_add_scalar_to_vector)
-{
-	const int vSize = 6, addVal = 7;
-	TVector<int> v1(vSize), v2(vSize), res(vSize);
-	for(int i = 0; i < v1.GetSize(); i++)
-		v1[i] = i+1;
-	v2 = v1;
-	v1 = v1 + addVal;
-	res = v1 - v2;
-	for(int i = 0; i < res.GetSize(); i++)
-		EXPECT_TRUE(addVal == res[i]);
+TEST(TVector, can_add_scalar_to_vector) {
+  const int vSize = 6, addVal = 7;
+  TVector<int> v1(vSize), v2(vSize), res(vSize);
+  for (int i = 0; i < v1.GetSize(); i++)
+    v1[i] = i+1;
+  v2 = v1;
+  v1 = v1 + addVal;
+  res = v1 - v2;
+  for (int i = 0; i < res.GetSize(); i++)
+    EXPECT_TRUE(addVal == res[i]);
 }
-
-TEST(TVector, can_subtract_scalar_from_vector)
-{
-	const int vSize = 6, addVal = 7;
-	TVector<int> v1(vSize), v2(vSize), res(vSize);
-	for(int i = 0; i < v1.GetSize(); i++)
-		v1[i] = i+1;
-	v2 = v1;
-	v1 = v1 - addVal;
-	res = v2 - v1;
-	for(int i = 0; i < res.GetSize(); i++)
-		EXPECT_EQ(addVal, res[i]);
+TEST(TVector, can_subtract_scalar_from_vector) {
+  const int vSize = 6, addVal = 7;
+  TVector<int> v1(vSize), v2(vSize), res(vSize);
+  for (int i = 0; i < v1.GetSize(); i++)
+    v1[i] = i+1;
+  v2 = v1;
+  v1 = v1 - addVal;
+  res = v2 - v1;
+  for (int i = 0; i < res.GetSize(); i++)
+    EXPECT_EQ(addVal, res[i]);
 }
-
-TEST(TVector, can_multiply_scalar_by_vector)
-{
-	const int vSize = 6, mulVal = 7;
-	TVector<int> v1(vSize), res(vSize);
-	res = v1 * mulVal;
-	for(int i = 0; i < res.GetSize(); i++)
-		EXPECT_EQ(v1[i] * mulVal, res[i]);
+TEST(TVector, can_multiply_scalar_by_vector) {
+  const int vSize = 6, mulVal = 7;
+  TVector<int> v1(vSize), res(vSize);
+  res = v1 * mulVal;
+  for (int i = 0; i < res.GetSize(); i++)
+    EXPECT_EQ(v1[i] * mulVal, res[i]);
 }
-
-TEST(TVector, can_add_vectors_with_equal_size)
-{
-	const int vSize = 6;
-	TVector<int> v1(6), v2(6), res(6);
-	for(int i = 0; i < v1.GetSize(); i++)
-	{
-		v1[i] = i;
-		v2[i] = 6 - i;
-	}
-	res = v1 + v2;
-	for(int i = 0; i < res.GetSize(); i++)
-		EXPECT_EQ(v1.GetSize(), res[i]);
+TEST(TVector, can_add_vectors_with_equal_size) {
+  const int vSize = 6;
+  TVector<int> v1(6), v2(6), res(6);
+  for (int i = 0; i < v1.GetSize(); i++) {
+    v1[i] = i;
+    v2[i] = 6 - i;
+  }
+  res = v1 + v2;
+  for (int i = 0; i < res.GetSize(); i++)
+    EXPECT_EQ(v1.GetSize(), res[i]);
 }
-
-TEST(TVector, cant_add_vectors_with_not_equal_size)
-{
-	TVector<int> v1(4), v2(7), res(8);
-	v1[0] = 2;
-	v2[0] = 3;
-	ASSERT_ANY_THROW(res = v1 + v2);
+TEST(TVector, cant_add_vectors_with_not_equal_size) {
+  TVector<int> v1(4), v2(7), res(8);
+  v1[0] = 2;
+  v2[0] = 3;
+  ASSERT_ANY_THROW(res = v1 + v2);
 }
-
-TEST(TVector, can_subtract_vectors_with_equal_size)
-{
-	const int vSize = 6;
-	TVector<int> v1(vSize), v2(vSize), res(vSize);
-	for(int i = 0; i < v1.GetSize(); i++)
-	{
-		v1[i] = 0;
-		v2[i] = i;
-	}
-	res = v2 - v1;
-	for(int i = 0; i < res.GetSize(); i++)
-		EXPECT_EQ(i, res[i]);
+TEST(TVector, can_subtract_vectors_with_equal_size) {
+  const int vSize = 6;
+  TVector<int> v1(vSize), v2(vSize), res(vSize);
+  for (int i = 0; i < v1.GetSize(); i++) {
+    v1[i] = 0;
+    v2[i] = i;
+  }
+  res = v2 - v1;
+  for (int i = 0; i < res.GetSize(); i++)
+    EXPECT_EQ(i, res[i]);
 }
-
-TEST(TVector, cant_subtract_vectors_with_not_equal_size)
-{
-	TVector<int> v1(4), v2(7), res(8);
-	v1[0] = 2;
-	v2[0] = 3;
-	ASSERT_ANY_THROW(res = v1 - v2);
+TEST(TVector, cant_subtract_vectors_with_not_equal_size) {
+  TVector<int> v1(4), v2(7), res(8);
+  v1[0] = 2;
+  v2[0] = 3;
+  ASSERT_ANY_THROW(res = v1 - v2);
 }
-
-TEST(TVector, can_multiply_vectors_with_equal_size)
-{
-	TVector<int> v1(6), v2(6);
-	int sum = 0;	
-	for(int i = 0; i < 6; i++)
-	{
-		v1[i] = v2[i] = i; // 0*0 1*1 2*2 3*3
-		sum += i*i; 
-	}
-	EXPECT_EQ(v1 * v2, sum);
+TEST(TVector, can_multiply_vectors_with_equal_size) {
+  TVector<int> v1(6), v2(6);
+  int sum = 0;
+  for (int i = 0; i < 6; i++) {
+    v1[i] = v2[i] = i; // 0*0 1*1 2*2 3*3
+    sum += i*i;
+  }
+  EXPECT_EQ(v1 * v2, sum);
 }
-
-TEST(TVector, cant_multiply_vectors_with_not_equal_size)
-{
-	TVector<int> v1(4), v2(7), res(8);
-	v1[0] = 2;
-	v2[0] = 3;
-	ASSERT_ANY_THROW(res = v1 * v2);
+TEST(TVector, cant_multiply_vectors_with_not_equal_size) {
+  TVector<int> v1(4), v2(7), res(8);
+  v1[0] = 2;
+  v2[0] = 3;
+  ASSERT_ANY_THROW(res = v1 * v2);
 }
-
