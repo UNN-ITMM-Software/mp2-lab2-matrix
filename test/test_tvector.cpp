26c3ea6ae24ef67d2767fec93deb84f678f6e0f6
Alexander Sysoyev <sysoyev@vmk.unn.ru>
2022-09-22 17:14:52 +0300

    upgrade code to new version

--- test/test_tvector.cpp
+++ test/test_tvector.cpp
@@ -1,152 +1,140 @@
-#include "utmatrix.h"
+#include "tmatrix.h"
 
 #include <gtest.h>
 
-TEST(TVector, can_create_vector_with_positive_length)
+TEST(TDynamicVector, can_create_vector_with_positive_length)
 {
-  ASSERT_NO_THROW(TVector<int> v(5));
+  ASSERT_NO_THROW(TDynamicVector<int> v(5));
 }
 
-TEST(TVector, cant_create_too_large_vector)
+TEST(TDynamicVector, cant_create_too_large_vector)
 {
-  ASSERT_ANY_THROW(TVector<int> v(MAX_VECTOR_SIZE + 1));
+  ASSERT_ANY_THROW(TDynamicVector<int> v(MAX_VECTOR_SIZE + 1));
 }
 
-TEST(TVector, throws_when_create_vector_with_negative_length)
+TEST(TDynamicVector, throws_when_create_vector_with_negative_length)
 {
-  ASSERT_ANY_THROW(TVector<int> v(-5));
+  ASSERT_ANY_THROW(TDynamicVector<int> v(-5));
 }
 
-TEST(TVector, throws_when_create_vector_with_negative_startindex)
+TEST(TDynamicVector, can_create_copied_vector)
 {
-  ASSERT_ANY_THROW(TVector<int> v(5, -2));
+  TDynamicVector<int> v(10);
+
+  ASSERT_NO_THROW(TDynamicVector<int> v1(v));
 }
 
-TEST(TVector, can_create_copied_vector)
-{
-  TVector<int> v(10);
-
-  ASSERT_NO_THROW(TVector<int> v1(v));
-}
-
-TEST(TVector, copied_vector_is_equal_to_source_one)
+TEST(TDynamicVector, copied_vector_is_equal_to_source_one)
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, copied_vector_has_its_own_memory)
+TEST(TDynamicVector, copied_vector_has_its_own_memory)
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, can_get_size)
+TEST(TDynamicVector, can_get_size)
 {
-  TVector<int> v(4);
+  TDynamicVector<int> v(4);
 
-  EXPECT_EQ(4, v.GetSize());
+  EXPECT_EQ(4, v.size());
 }
 
-TEST(TVector, can_get_start_index)
-{
-  TVector<int> v(4, 2);
+//TEST(TDynamicVector, can_set_and_get_element)
+//{
+//  TDynamicVector<int> v(4);
+//  v[0] = 4;
+//
+//  EXPECT_EQ(4, v[0]);
+//}
 
-  EXPECT_EQ(2, v.GetStartIndex());
-}
-
-TEST(TVector, can_set_and_get_element)
-{
-  TVector<int> v(4);
-  v[0] = 4;
-
-  EXPECT_EQ(4, v[0]);
-}
-
-TEST(TVector, throws_when_set_element_with_negative_index)
+TEST(TDynamicVector, throws_when_set_element_with_negative_index)
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, throws_when_set_element_with_too_large_index)
+TEST(TDynamicVector, throws_when_set_element_with_too_large_index)
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, can_assign_vector_to_itself)
+TEST(TDynamicVector, can_assign_vector_to_itself)
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, can_assign_vectors_of_equal_size)
+TEST(TDynamicVector, can_assign_vectors_of_equal_size)
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, assign_operator_change_vector_size)
+TEST(TDynamicVector, assign_operator_change_vector_size)
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, can_assign_vectors_of_different_size)
+TEST(TDynamicVector, can_assign_vectors_of_different_size)
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, compare_equal_vectors_return_true)
+TEST(TDynamicVector, compare_equal_vectors_return_true)
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, compare_vector_with_itself_return_true)
+TEST(TDynamicVector, compare_vector_with_itself_return_true)
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, vectors_with_different_size_are_not_equal)
+TEST(TDynamicVector, vectors_with_different_size_are_not_equal)
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, can_add_scalar_to_vector)
+TEST(TDynamicVector, can_add_scalar_to_vector)
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, can_subtract_scalar_from_vector)
+TEST(TDynamicVector, can_subtract_scalar_from_vector)
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, can_multiply_scalar_by_vector)
+TEST(TDynamicVector, can_multiply_scalar_by_vector)
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, can_add_vectors_with_equal_size)
+TEST(TDynamicVector, can_add_vectors_with_equal_size)
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, cant_add_vectors_with_not_equal_size)
+TEST(TDynamicVector, cant_add_vectors_with_not_equal_size)
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, can_subtract_vectors_with_equal_size)
+TEST(TDynamicVector, can_subtract_vectors_with_equal_size)
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, cant_subtract_vectors_with_not_equal_size)
+TEST(TDynamicVector, cant_subtract_vectors_with_not_equal_size)
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, can_multiply_vectors_with_equal_size)
+TEST(TDynamicVector, can_multiply_vectors_with_equal_size)
 {
   ADD_FAILURE();
 }
 
-TEST(TVector, cant_multiply_vectors_with_not_equal_size)
+TEST(TDynamicVector, cant_multiply_vectors_with_not_equal_size)
 {
   ADD_FAILURE();
 }
